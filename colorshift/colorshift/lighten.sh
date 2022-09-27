e4d198302a377e42d3f89470312a53dfda6a3973
Marvin Jarju <marvin.jarju@gmail.com>
2022-09-27 10:01:07 +0200

    add colorshift

---
+++ colorshift/colorshift/lighten.sh
@@ -0,0 +1,8 @@
+this_file_parent_dir=~/.config/colorshift
+
+# export theme for vim
+export CONSOLE_THEME="light"
+
+echo "$this_file_parent_dir/darken.sh" > ~/.config/colorshift/target_file.txt
+
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=180"
